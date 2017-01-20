a6d97f6a9f6e473d5d9940b7308554ff3c2caa0c
Oleg Sovetnik <sovetnik@oblaka.biz>
2017-01-21 00:32:58 +0300

    added books#index

---
+++ spec/web/controllers/books/index_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require_relative '../../../../apps/web/controllers/books/index'
+
+describe Web::Controllers::Books::Index do
+  let(:action) { Web::Controllers::Books::Index.new }
+  let(:params) { Hash[] }
+  let(:repo) { BookRepository.new }
+
+  before do
+    repo.clear
+
+    @book = repo.create(title: 'TDD', author: 'Kent Beck')
+  end
+
+  it 'is successful' do
+    response = action.call(params)
+    response[0].must_equal 200
+  end
+
+  it 'exposes all books' do
+    action.call(params)
+    action.exposures[:books].must_equal [@book]
+  end
+end
